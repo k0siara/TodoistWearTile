0f554b8b0b0c1a7724d6ef9d0d34ec1cd1dcd77e
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2022-05-20 01:17:57 +0200

    Removes unnecessary param from GetAccessTokenRequest

--- app/src/main/java/com/patrykkosieradzki/todoist/wear/tile/network/GetAccessTokenRequest.kt
+++ app/src/main/java/com/patrykkosieradzki/todoist/wear/tile/network/GetAccessTokenRequest.kt
@@ -7,6 +7,5 @@ import com.squareup.moshi.JsonClass
 data class GetAccessTokenRequest(
     @Json(name = "client_id") val clientId: String,
     @Json(name = "client_secret") val clientSecret: String,
-    @Json(name = "code") val code: String,
-    @Json(name = "redirect_uri") val redirectUri: String = "https://wear.googleapis.com/3p_auth/com.patrykkosieradzki.todoist.wear.tile"
+    @Json(name = "code") val code: String
 )
